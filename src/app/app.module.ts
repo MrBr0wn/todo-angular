fd17a7689101316d46e706531a02014c9646e263
Mr. Brown <gitcherrypick.work@gmail.com>
2022-04-08 17:53:44 +0500

    Adding Material UI. Creating components: header, project, todo-list, modal-window. Preparing templates: header, project, todo-list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ProjectComponent } from './project/project.component';
+import { TodoListComponent } from './project/todo-list/todo-list.component';
+import { ModalWindowComponent } from './modal-window/modal-window.component';
+import { FormAddComponent } from './modal-window/form-add/form-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjectComponent,
+    TodoListComponent,
+    ModalWindowComponent,
+    FormAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
